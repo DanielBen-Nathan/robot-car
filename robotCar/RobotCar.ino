810343862254105e731865602812a7ae4544e4ff
DanielBen-Nathan <dan.bennathan@hotmail.co.uk>
2021-03-07 17:44:57 +0000

    Formatting

--- robotCar/RobotCar.ino
+++ robotCar/RobotCar.ino
@@ -1,9 +1,8 @@
+#include <Servo.h> //servo library
+Servo myservo; // create servo object to control servo
 
-#include <Servo.h>  //servo library
-Servo myservo;      // create servo object to control servo
-
-int Echo = A4;  
-int Trig = A5; 
+int Echo = A4;
+int Trig = A5;
 
 #define ENA 5
 #define ENB 6
@@ -24,86 +23,92 @@ float forward_sensor_ratio = 1.75;
 int reverse_time_forward = 300;
 int reverse_time_side = 350;
 
-
-void forward(){ 
-  analogWrite(ENA, carSpeed);
-  analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  Serial.println("Forward");
+void forward()
+{
+    analogWrite(ENA, carSpeed);
+    analogWrite(ENB, carSpeed);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    Serial.println("Forward");
 }
 
-void back() {
-  analogWrite(ENA, carSpeed);
-  analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  Serial.println("Back");
+void back()
+{
+    analogWrite(ENA, carSpeed);
+    analogWrite(ENB, carSpeed);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    Serial.println("Back");
 }
 
-void left() {
-  analogWrite(ENA, carturnSpeed);
-  analogWrite(ENB, carturnSpeed);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH); 
-  Serial.println("Left");
+void left()
+{
+    analogWrite(ENA, carturnSpeed);
+    analogWrite(ENB, carturnSpeed);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    Serial.println("Left");
 }
 
-void right() {
-  analogWrite(ENA, carturnSpeed);
-  analogWrite(ENB, carturnSpeed);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  Serial.println("Right");
+void right()
+{
+    analogWrite(ENA, carturnSpeed);
+    analogWrite(ENB, carturnSpeed);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    Serial.println("Right");
 }
 
-void stop() {
-  digitalWrite(ENA, LOW);
-  digitalWrite(ENB, LOW);
-  Serial.println("Stop!");
-} 
+void stop()
+{
+    digitalWrite(ENA, LOW);
+    digitalWrite(ENB, LOW);
+    Serial.println("Stop!");
+}
 
 //Ultrasonic distance measurement Sub function
-int Distance_test() {
-  digitalWrite(Trig, LOW);   
-  delayMicroseconds(2);
-  digitalWrite(Trig, HIGH);  
-  delayMicroseconds(20);
-  digitalWrite(Trig, LOW);   
-  float Fdistance = pulseIn(Echo, HIGH);  
-  Fdistance= Fdistance / 58;       
-  return (int)Fdistance;
-}  
-
-void setup() { 
-  myservo.attach(3,700,2400);  // attach servo on pin 3 to servo object
-  Serial.begin(9600);     
-  pinMode(Echo, INPUT);    
-  pinMode(Trig, OUTPUT);  
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  stop();
-  forward();
+int Distance_test()
+{
+    digitalWrite(Trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trig, HIGH);
+    delayMicroseconds(20);
+    digitalWrite(Trig, LOW);
+    float Fdistance = pulseIn(Echo, HIGH);
+    Fdistance = Fdistance / 58;
+    return (int)Fdistance;
 }
 
+void setup()
+{
+    myservo.attach(3, 700, 2400); // attach servo on pin 3 to servo object
+    Serial.begin(9600);
+    pinMode(Echo, INPUT);
+    pinMode(Trig, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    stop();
+    forward();
+}
 
-int check_reverse(int sensor_dir, int back_time){
+int check_reverse(int sensor_dir, int back_time)
+{
     myservo.write(sensor_dir);
     delay(360);
     int distance = Distance_test();
-    if (distance < (sensor_distance * reverse_sensor_ratio)){
+    if (distance < (sensor_distance * reverse_sensor_ratio)) {
         back();
         delay(back_time);
         stop();
@@ -111,19 +116,20 @@ int check_reverse(int sensor_dir, int back_time){
     return distance;
 }
 
-
-void turn(int turn_time, void (*direction)()){
-    for ( ; ; ){
+void turn(int turn_time, void (*direction)())
+{
+    for (;;) {
         direction();
         delay(turn_time);
         int distance_mid = Distance_test();
-        if (distance_mid > sensor_distance * forward_sensor_ratio){
-            break; 
+        if (distance_mid > sensor_distance * forward_sensor_ratio) {
+            break;
         }
     }
 }
 
-void choose_dir(){
+void choose_dir()
+{
     stop();
     check_reverse(90, reverse_time_forward);
 
@@ -133,37 +139,37 @@ void choose_dir(){
     myservo.write(90);
     delay(360);
     int turn_time = 35;
-    if(distance_right > distance_left) {
+    if (distance_right > distance_left) {
         turn(turn_time, &right);
     }
-    else if(distance_right < distance_left) {
+    else if (distance_right < distance_left) {
         turn(turn_time, &left);
     }
-    else{
+    else {
         turn(turn_time, &left);
     }
 }
 
-void scan(int sensor_pos){
+void scan(int sensor_pos)
+{
     myservo.write(sensor_pos);
     delay(sensor_delay);
     distance = Distance_test();
-    if (distance <= sensor_distance){
-      choose_dir();
+    if (distance <= sensor_distance) {
+        choose_dir();
     }
-    else{
-      forward();
+    else {
+        forward();
     }
 }
 
-
-void loop() {
+void loop()
+{
     int sensor_pos;
-    for (sensor_pos=90-sensor_angle; sensor_pos < 90+sensor_angle; sensor_pos+=sensor_step){  
-      scan(sensor_pos);
+    for (sensor_pos = 90 - sensor_angle; sensor_pos < 90 + sensor_angle; sensor_pos += sensor_step) {
+        scan(sensor_pos);
     }
-    for (sensor_pos=90+sensor_angle; sensor_pos > 90-sensor_angle; sensor_pos-=sensor_step){ 
-     scan(sensor_pos);
+    for (sensor_pos = 90 + sensor_angle; sensor_pos > 90 - sensor_angle; sensor_pos -= sensor_step) {
+        scan(sensor_pos);
     }
-
 }
